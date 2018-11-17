b245a6f084328cf19603cd0d14150b3a78ac250d
Cara Morelli <caramorelli7@gmail.com>
2018-11-17 02:06:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const companyName = 'Scuber';
 
 let mostProfitableNeighborhoodtest = 'Chelsea';
 
+let mostProfitableNeighborhoodtest = 'Upper West Side'
+
 // function mostProfitableNeighborhood() {
 //   let mostProfitableNeighborhoodtest = 'Chelsea';
 // }
